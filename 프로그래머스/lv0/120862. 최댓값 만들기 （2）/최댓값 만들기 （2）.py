76823c0bb91d793a52532b993ce5386e63155a17
GaYoNE <91003766+GaYoNE00@users.noreply.github.com>
2023-04-08 11:48:11 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = numbers[0]*numbers[1]
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i == j:
+                continue
+            elif numbers[i]*numbers[j] > answer:
+                answer = numbers[i]*numbers[j]
+    return answer
