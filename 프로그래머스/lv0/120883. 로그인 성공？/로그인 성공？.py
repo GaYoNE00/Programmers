5442f45b23fa91c237115428452e3da142c9df6d
GaYoNE <91003766+GaYoNE00@users.noreply.github.com>
2023-04-10 11:23:28 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,8 @@
+def solution(id_pw, db):
+    answer = 'fail'
+    for i in db:
+        if id_pw == i:
+            answer = 'login'
+        elif id_pw[0] == i[0] and id_pw[1] != i[1]:
+            answer = "wrong pw"
+    return answer
